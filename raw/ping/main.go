8254fbe03d7f9786e533a21d973275b098f65625
Gabor Tanz <gabor.tanz@gmail.com>
2017-12-04 22:33:50 +0100

    add ping with raw sockets

---
+++ raw/ping/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"github.com/izolight/go-network-programming/util"
+	"net"
+	"os"
+)
+
+type IcmpPacket struct {
+	Type 		int
+	Code		int
+	Checksum	int
+	Identifier 	int
+	Sequence 	int
+	Message 	[]byte
+}
+
+func main() {
+	addr, err := net.ResolveIPAddr("ip", os.Args[1])
+	if err != nil {
+		fmt.Printf("Can't resolve %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialIP("ip4:icmp", nil, addr)
+	util.CheckError(err)
+
+	var pkt IcmpPacket
+	pkt.Type = 8
+	pkt.Code = 0
+	pkt.Identifier = 12
+	pkt.Sequence = 34
+	pkt.Message = []byte("test1234")
+
+	msg := pkt.convertToByte()
+
+	_, err = conn.Write(msg)
+	util.CheckError(err)
+
+	_, err = conn.Read(msg[0:])
+	util.CheckError(err)
+
+	fmt.Println("Got response")
+	fmt.Println(msg)
+
+	os.Exit(0)
+}
+
+func (i *IcmpPacket)convertToByte() []byte {
+	b := []byte{byte(i.Type), byte(i.Code), 0, 0, byte(i.Identifier), byte(i.Identifier >> 8), byte(i.Sequence), byte(i.Sequence >> 8)}
+	if i.Message != nil {
+		b = append(b, i.Message...)
+	}
+	checksum := checkSum(b)
+	b[2] = byte(checksum)
+	b[3] = byte(checksum >> 8)
+	return b
+}
+
+func checkSum(b []byte) uint16 {
+	csumcv := len(b) - 1 // checksum coverage
+	s := uint32(0)
+	for i := 0; i < csumcv; i += 2 {
+		s += uint32(b[i+1])<<8 | uint32(b[i])
+	}
+	if csumcv&1 == 0 {
+		s += uint32(b[csumcv])
+	}
+	s = s>>16 + s&0xffff
+	s = s + s>>16
+	return ^uint16(s)
+}
