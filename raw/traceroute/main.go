8af6e084dd06c9f591225c5e2d898eccfec61490
Gabor Tanz <gabor.tanz@gmail.com>
2017-12-07 19:02:41 +0100

    split cleaner into headers and packet and give correct names

--- raw/traceroute/main.go
+++ raw/traceroute/main.go
@@ -9,33 +9,33 @@ import (
 )
 
 // from golan.org/x/net/ipv4/header.go
-type Header struct {
-	Version  int    // protocol version
-	Len      int    // header length
-	TOS      int    // type-of-service
-	TotalLen int    // packet total length
-	ID       int    // identification
-	Flags    int    // flags
-	FragOff  int    // fragment offset
-	TTL      int    // time-to-live
-	Protocol int    // next protocol
-	Checksum int    // checksum
-	Src      net.IP // source address
-	Dst      net.IP // destination address
+type IPHeader struct {
+	Version        int    // protocol version
+	IHL            int    // header length
+	ToS            int    // type-of-service
+	TotalLength    int    // packet total length
+	ID             int    // identification
+	Flags          int    // flags
+	FragmentOffset int    // fragment offset
+	TTL            int    // time-to-live
+	Protocol       int    // next protocol
+	Checksum       int    // checksum
+	Source         net.IP // source address
+	Destination    net.IP // destination address
 }
 
-type IcmpPacket struct {
+type ICMPHeader struct {
 	Type       int
 	Code       int
 	Checksum   int
 	Identifier int
 	Sequence   int
-	Message    []byte
 }
 
-type IPPacket struct {
-	Header Header
-	Data   IcmpPacket
+type Packet struct {
+	IPHeader   IPHeader
+	ICMPHeader ICMPHeader
+	Data       []byte
 }
 
 func main() {
@@ -52,22 +52,21 @@ func main() {
 		Port: 0,
 		Addr: [4]byte{8, 8, 8, 8},
 	}
-	h := Header{}
-	h.defaultHeader()
-	h.Dst = net.IPv4(addr.Addr[0], addr.Addr[1], addr.Addr[2], addr.Addr[3])
-	d := IcmpPacket{}
-	d.defaultPacket()
+	iph := NewIPHeader()
+	iph.Destination = net.IPv4(addr.Addr[0], addr.Addr[1], addr.Addr[2], addr.Addr[3])
+	icmph := NewICMPHeader()
 
-	p := IPPacket{
-		Header: h,
-		Data:   d,
-	}
+	p := NewPacket(&iph, &icmph, []byte("test"))
 
-	responses := make([]IPPacket, 20)
-	for ttl := 1; ttl < 20; ttl++ {
-		p.Header.TTL = ttl
-		p.Data.Identifier = ttl
-		err = syscall.Sendto(rawsock, p.toBytes(), 0, &addr)
+	//responses := make([]Packet, 10)
+	for ttl := 1; ttl < 2; ttl++ {
+		p.IPHeader.TTL = ttl
+		p.ICMPHeader.Identifier = ttl
+		payload, err := p.MarshalBinary()
+		if err != nil {
+			log.Fatalf("Error marshalling packet %s", err)
+		}
+		err = syscall.Sendto(rawsock, payload, 0, &addr)
 		if err != nil {
 			log.Fatal("Sendto:", err)
 		}
@@ -78,44 +77,134 @@ func main() {
 		if err != nil {
 			log.Fatalf("Error receiving data: %s", err)
 		}
-		responses[ttl] = fromBytes(buf[:n])
-		fmt.Println(responses[ttl].Header, responses[ttl].Data)
+		// responses[ttl] = fromBytes(buf[:n])
+		fmt.Println(buf[:n])
+		//fmt.Println(responses[ttl].Header, responses[ttl].Data)
 		//fmt.Println(i)
 		//fmt.Printf("% 02x\n", buf[:n])
 	}
 }
 
-func (h *Header) defaultHeader() {
-	h.Version = 4    // ipv4
-	h.Len = 5        // default
-	h.TOS = 0        // best effort
-	h.Flags = 1 << 1 // don't fragment
-	h.FragOff = 0
-	h.TTL = 1
-	h.Protocol = 1 // icmp
+
+
+func (p Packet) MarshalBinary() ([]byte, error) {
+	enc := make([]byte, p.IPHeader.TotalLength)
+	h, err := p.IPHeader.MarshalBinary()
+	if err != nil {
+		log.Fatalf("Error marshalling ip header %s\n", err)
+	}
+	copy(enc[:4*p.IPHeader.IHL], h)
+	i, err := p.ICMPHeader.MarshalBinary()
+	if err != nil {
+		log.Fatalf("Error marshalling icmp header %s\n", err)
+	}
+	copy(enc[4*p.IPHeader.IHL:p.IPHeader.IHL*4+8], i)
+	copy(enc[p.IPHeader.IHL*4+8:p.IPHeader.TotalLength], p.Data)
+	return enc, nil
 }
 
-func (h Header) String() string {
-	s := fmt.Sprintf("Version: %v, Len: %v, TOS: %v, TotalLen: %v, ID: %v, Flags: %v, FragOffset: %v," +
-		" TTL: %v, Protocol: %v, Checksum: %v, Src: %v, Dst: %v",
-			h.Version, h.Len, h.TOS, h.TotalLen, h.ID, h.Flags, h.FragOff, h.TTL, h.Protocol, h.Checksum, h.Src, h.Dst)
+func (ih IPHeader) String() string {
+	s := fmt.Sprintf("Version: %v, IHL: %v, ToS: %v, TotalLen: %v, ID: %v, Flags: %v, FragOffset: %v,"+
+		" TTL: %v, Protocol: %v, Checksum: %v, Source: %v, Destination: %v\n",
+		ih.Version, ih.IHL, ih.ToS, ih.TotalLength, ih.ID, ih.Flags, ih.FragmentOffset, ih.TTL, ih.Protocol,
+		ih.Checksum, ih.Source, ih.Destination)
 	return s
 }
 
-func (i IcmpPacket) String() string {
-	s := fmt.Sprintf("Type: %v, Code: %v, Checksum: %v, Identifier: %v, Sequence: %v, Message: %v",
-		i.Type, i.Code, i.Checksum, i.Identifier, i.Sequence, i.Message)
+func (ih IPHeader) MarshalBinary() ([]byte, error) {
+	enc := make([]byte, 20)
+	enc[0] = byte(ih.Version<<4 | ih.IHL)
+	enc[1] = byte(ih.ToS)
+	binary.BigEndian.PutUint16(enc[2:4], uint16(ih.TotalLength))
+	binary.BigEndian.PutUint16(enc[4:6], uint16(ih.ID))
+	flagsAndFragOff := (ih.FragmentOffset & 0x1fff) | int(ih.Flags<<13)
+	binary.BigEndian.PutUint16(enc[6:8], uint16(flagsAndFragOff))
+	enc[8] = byte(ih.TTL)
+	enc[9] = byte(ih.Protocol)
+	binary.BigEndian.PutUint16(enc[10:12], uint16(ih.Checksum))
+	copy(enc[12:16], ih.Source)
+	copy(enc[16:20], ih.Destination[12:])
+
+	return enc, nil
+}
+
+func (ih ICMPHeader) String() string {
+	s := fmt.Sprintf("Type: %v, Code: %v, Checksum: %v",
+		ih.Type, ih.Code, ih.Checksum)
+	switch ih.Type {
+	case 0, 8, 13, 14, 15, 16:
+		s = fmt.Sprintf("%s, Indentifier: %s, Sequence: %s\n", s, ih.Identifier, ih.Sequence)
+	}
 	return s
 }
 
-func fromBytes(b [] byte) IPPacket{
+func (ih ICMPHeader) MarshalBinary() ([]byte, error) {
+	enc := make([] byte, 8)
+	enc[0] = byte(ih.Type)
+	enc[1] = byte(ih.Code)
+	enc[2] = byte(ih.Checksum)
+	enc[3] = byte(ih.Checksum >> 8)
+	switch ih.Type {
+	case 0, 8, 13, 14, 15, 16:
+		enc[4] = byte(ih.Identifier)
+		enc[5] = byte(ih.Identifier >> 8)
+		enc[6] = byte(ih.Sequence)
+		enc[7] = byte(ih.Sequence >> 8)
+	default:
+		copy(enc[4:8], []byte{0, 0, 0, 0})
+	}
+
+	return enc, nil
+}
+
+func NewPacket(h *IPHeader, i *ICMPHeader, data []byte) Packet {
+	payload := make([]byte, 8+len(data))
+	ib, err := i.MarshalBinary()
+	if err != nil {
+		log.Fatalf("Error marshaling to bytes: %s", err)
+	}
+	copy(payload[:8], ib)
+	copy(payload[8:], data)
+	checksum := checkSum(payload)
+	i.Checksum = int(checksum)
+	h.TotalLength = h.IHL*4 + 8 + len(data)
+
+	return Packet{
+		IPHeader:   *h,
+		ICMPHeader: *i,
+		Data:       data,
+	}
+}
+
+func NewIPHeader() IPHeader {
+	return IPHeader{
+		Version:        4,      // ipv4
+		IHL:            5,      // default
+		ToS:            0,      // best effort
+		Flags:          1 << 1, // don't fragment
+		FragmentOffset: 0,
+		TTL:            1,
+		Protocol:       1, // icmp
+	}
+}
+
+func NewICMPHeader() ICMPHeader {
+	return ICMPHeader{
+		Type:       8, // Echo Request
+		Code:       0,
+		Identifier: 1,
+		Sequence:   1,
+	}
+}
+
+/*func fromBytes(b [] byte) IPPacket {
 	var i IPPacket
 	i.Header.Version = int(b[0] >> 4)
 	i.Header.Len = int(b[0] & 0x0f)
 	i.Header.TOS = int(b[1])
 	i.Header.TotalLen = int(binary.BigEndian.Uint16(b[2:4]))
 	i.Header.ID = int(binary.BigEndian.Uint16(b[4:6]))
-	i.Header.Flags = int(b[7]>>5)
+	i.Header.Flags = int(b[7] >> 5)
 	b[7] = b[7] & 0x1f
 	i.Header.FragOff = int(binary.BigEndian.Uint16([]byte(b[6:8])))
 	i.Header.TTL = int(b[8])
@@ -123,7 +212,7 @@ func fromBytes(b [] byte) IPPacket{
 	i.Header.Checksum = int(binary.BigEndian.Uint16(b[10:12]))
 	i.Header.Src = net.IPv4(b[12], b[13], b[14], b[15])
 	i.Header.Dst = net.IPv4(b[16], b[17], b[18], b[19])
-// Works atm for ttl message
+	// Works atm for ttl message
 	i.Data.Type = int(b[20])
 	i.Data.Code = int(b[21])
 	i.Data.Checksum = int(binary.LittleEndian.Uint16(b[22:24]))
@@ -132,47 +221,7 @@ func fromBytes(b [] byte) IPPacket{
 	//copy(i.Data.Message, b[52:])
 
 	return i
-}
-
-func (i *IcmpPacket) defaultPacket() {
-	i.Type = 8
-	i.Code = 0
-	i.Identifier = 1
-	i.Sequence = 1
-	i.Message = []byte("Test")
-}
-
-func (p *IPPacket) toBytes() []byte {
-	data := p.Data.toBytes()
-	p.Header.TotalLen = 20 + len(data)
-	b := make([]byte, p.Header.TotalLen)
-	b[0] = byte(p.Header.Version<<4 | p.Header.Len)
-	b[1] = byte(p.Header.TOS)
-	b[2] = byte(p.Header.TotalLen)
-	binary.BigEndian.PutUint16(b[2:4], uint16(p.Header.TotalLen))
-	binary.BigEndian.PutUint16(b[4:6], uint16(p.Header.ID))
-	flagsAndFragOff := (p.Header.FragOff & 0x1fff) | int(p.Header.Flags<<13)
-	binary.BigEndian.PutUint16(b[6:8], uint16(flagsAndFragOff))
-	b[8] = byte(p.Header.TTL)
-	b[9] = byte(p.Header.Protocol)
-	binary.BigEndian.PutUint16(b[10:12], uint16(p.Header.Checksum))
-	copy(b[12:16], p.Header.Src)
-	copy(b[16:20], p.Header.Dst[12:])
-	copy(b[20:p.Header.TotalLen], data)
-
-	return b
-}
-
-func (i *IcmpPacket) toBytes() []byte {
-	b := []byte{byte(i.Type), byte(i.Code), 0, 0, byte(i.Identifier), byte(i.Identifier >> 8), byte(i.Sequence), byte(i.Sequence >> 8)}
-	if i.Message != nil {
-		b = append(b, i.Message...)
-	}
-	checksum := checkSum(b)
-	b[2] = byte(checksum)
-	b[3] = byte(checksum >> 8)
-	return b
-}
+}*/
 
 func checkSum(b []byte) uint16 {
 	csumcv := len(b) - 1 // checksum coverage
